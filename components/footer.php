a2e9e81e19227593c12ade4756dcc4621f23834d
AmenAllahBelhouichet <101362318+AmenAllahBelhouichet@users.noreply.github.com>
2023-05-08 14:19:54 +0100

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+   &copy; copyright @ <?= date('Y'); ?> by <span>Eya hammami</span> | all rights reserved!
+</footer>
---
+++ components/user_header.php
@@ -0,0 +1,40 @@
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<header class="header">
+
+   <section class="flex">
+
+      
+
+      <form action="search.php" method="POST" class="search-form">
+         <input type="text" name="search_box" class="box" maxlength="100" placeholder="search for blogs" required>
+         <button type="submit" class="fas fa-search" name="search_btn"></button>
+      </form>
+
+      <div class="icons">
+         
+         <div id="search-btn" class="fas fa-search"></div>
+       
+      </div>
+
+      
+      
+         
+         
+         
+     
+
+   </section>
+
+</header>
